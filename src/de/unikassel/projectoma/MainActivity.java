afd7e18dc8943d19f9a27a1637a529fb14169702
daniel <baniel.decker@gmail.com>
2014-01-11 17:47:23 +0100

    Added some basic debug output stuff

--- src/de/unikassel/projectoma/MainActivity.java
+++ src/de/unikassel/projectoma/MainActivity.java
@@ -1,15 +1,32 @@
 package de.unikassel.projectoma;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import java.util.ArrayList;
 
-public class MainActivity extends Activity {
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+public class MainActivity extends ListActivity {
+	
+	// LIST OF ARRAY STRINGS WHICH WILL SERVE AS LIST ITEMS
+	ArrayList<String> listItems = new ArrayList<String>();
+	
+	// DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE
+	// LISTVIEW
+	ArrayAdapter<String> adapter;
+	
+	// RECORDING HOW MANY TIMES THE BUTTON HAS BEEN CLICKED
+	int clickCounter = 0;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		adapter = new ArrayAdapter<String>(this,
+		        android.R.layout.simple_list_item_1, listItems);
+		setListAdapter(adapter);
 	}
 	
 	@Override
@@ -20,4 +37,8 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	
+	public void myClickMethod(View v) {
+		listItems.add("Clicked : " + clickCounter++);
+		adapter.notifyDataSetChanged();
+	}
 }
