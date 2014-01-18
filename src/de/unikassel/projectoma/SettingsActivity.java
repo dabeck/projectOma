0cc3fafdce6d3886dfe9efd09728a4608d99d5e2
daniel <baniel.decker@gmail.com>
2014-01-18 18:20:04 +0100

    added warning dialog

--- src/de/unikassel/projectoma/SettingsActivity.java
+++ src/de/unikassel/projectoma/SettingsActivity.java
@@ -1,7 +1,11 @@
 package de.unikassel.projectoma;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.view.View;
 
 public class SettingsActivity extends PreferenceActivity {
 	
@@ -11,5 +15,43 @@ public class SettingsActivity extends PreferenceActivity {
 		
 		addPreferencesFromResource(R.layout.settings);
 		
+		Preference btnReset = (Preference) findPreference("reset_game");
+		btnReset.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+			
+			@Override
+			public boolean onPreferenceClick(Preference arg0) {
+				btnResetClicked();
+				return true;
+			}
+		});
+
+	}
+	
+	public void btnResetClicked() {
+		new AlertDialog.Builder(this)
+		        .setTitle(R.string.reset_warning)
+		        .setMessage(R.string.reset_warning_msg)
+		        .setCancelable(false)
+		        .setPositiveButton(R.string.yes,
+		                new DialogInterface.OnClickListener() {
+			                
+			                public void onClick(DialogInterface dialog, int id) {
+				                // if this button is clicked, reset the
+								// game
+			                }
+		                })
+		        .setNegativeButton(R.string.no,
+		                new DialogInterface.OnClickListener() {
+			        
+			        public void onClick(DialogInterface dialog, int id) {
+				        // if this button is clicked, just close
+				        // the dialog box and do nothing
+				        dialog.cancel();
+			        }
+		                }).setIcon(android.R.drawable.ic_dialog_alert).show();
+	}
+	
+	public void togglePause(View v) {
+
 	}
 }
