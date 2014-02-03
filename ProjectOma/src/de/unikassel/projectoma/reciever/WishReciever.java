60f0cfac55266238ad22af31bbb55bd61225aef1
daniel <baniel.decker@gmail.com>
2014-02-03 16:56:43 +0100

    moved libraries into jars to minamize our project

--- ProjectOma/src/de/unikassel/projectoma/reciever/WishReciever.java
+++ ProjectOma/src/de/unikassel/projectoma/reciever/WishReciever.java
@@ -15,8 +15,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.preference.PreferenceManager;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.TaskStackBuilder;
 
 public class WishReciever extends BroadcastReceiver {
 
@@ -44,33 +42,33 @@ public class WishReciever extends BroadcastReceiver {
 	}
 
 	private void notify(Context context, Article wish) {
-		NotificationCompat.Builder mBuilder =
-		        new NotificationCompat.Builder(context)
-		        .setSmallIcon(R.drawable.ic_launcher)
-		        .setContentTitle(context.getString(R.string.notify_title))
-		        .setContentText(context.getString(wishToNotifyText(wish)));
-		// Creates an explicit intent for an Activity in your app
-		Intent resultIntent = new Intent(context, MainActivity.class);
-
-		// The stack builder object will contain an artificial back stack for the
-		// started Activity.
-		// This ensures that navigating backward from the Activity leads out of
-		// your application to the Home screen.
-		TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
-		// Adds the back stack for the Intent (but not the Intent itself)
-		stackBuilder.addParentStack(MainActivity.class);
-		// Adds the Intent that starts the Activity to the top of the stack
-		stackBuilder.addNextIntent(resultIntent);
-		PendingIntent resultPendingIntent =
-		        stackBuilder.getPendingIntent(
-		            0,
-		            PendingIntent.FLAG_UPDATE_CURRENT
-		        );
-		mBuilder.setContentIntent(resultPendingIntent);
-		NotificationManager mNotificationManager =
-		    (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-		// mId allows you to update the notification later on.
-		mNotificationManager.notify(0, mBuilder.build());
+//		NotificationCompat.Builder mBuilder =
+//		        new NotificationCompat.Builder(context)
+//		        .setSmallIcon(R.drawable.ic_launcher)
+//		        .setContentTitle(context.getString(R.string.notify_title))
+//		        .setContentText(context.getString(wishToNotifyText(wish)));
+//		// Creates an explicit intent for an Activity in your app
+//		Intent resultIntent = new Intent(context, MainActivity.class);
+//
+//		// The stack builder object will contain an artificial back stack for the
+//		// started Activity.
+//		// This ensures that navigating backward from the Activity leads out of
+//		// your application to the Home screen.
+//		TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+//		// Adds the back stack for the Intent (but not the Intent itself)
+//		stackBuilder.addParentStack(MainActivity.class);
+//		// Adds the Intent that starts the Activity to the top of the stack
+//		stackBuilder.addNextIntent(resultIntent);
+//		PendingIntent resultPendingIntent =
+//		        stackBuilder.getPendingIntent(
+//		            0,
+//		            PendingIntent.FLAG_UPDATE_CURRENT
+//		        );
+//		mBuilder.setContentIntent(resultPendingIntent);
+//		NotificationManager mNotificationManager =
+//		    (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+//		// mId allows you to update the notification later on.
+//		mNotificationManager.notify(0, mBuilder.build());
 	}
 	
 	private int wishToNotifyText(Article wish)
