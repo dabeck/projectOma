26bf0f3a2fa34d0dfb0d783ddc48e9dc81312c2b
daniel <baniel.decker@gmail.com>
2014-02-03 11:59:12 +0100

    removed duration stuff from imageHelper

--- ProjectOma/src/de/unikassel/projectoma/helper/ImageHelper.java
+++ ProjectOma/src/de/unikassel/projectoma/helper/ImageHelper.java
@@ -27,11 +27,9 @@ public class ImageHelper {
 	 * This method sets a new animated gif based on the given parameters.
 	 * 
 	 * @param img The name of the asset to load.
-	 * @param duration How long should this gif be played? (0 for infinite)
-	 * @param callback The callback gets called when the duration time is exceeded 
-	 * 					or instantly after setting the image on infinite images
+	 * @param callback The callback gets called when the image loading did finish
 	 */
-	private static void setOneTimeImg(final String img,final int duration, final Callback callback)
+	private static void setImage(final String img, final Callback callback)
 	{
 		if(appContext instanceof Context && img.contains(".gif") && !selectedImg.equals(img)){
 
@@ -62,47 +60,20 @@ public class ImageHelper {
 							loadingDialog.dismiss();
 							mainImage.setSource(gifImage);
 							mainImage.setFramesDuration(1);
-						}
-					});
-
-					if(duration == 0)
-					{
-						if(callback != null)
-						{
-							callback.onFinished();
-						}
-						
-						return;
-					}
-
-					long endTimeMillis = System.currentTimeMillis() + duration * 1000;
-					while (true)
-					{
-						if (System.currentTimeMillis() > endTimeMillis)
-						{
-
-							mainImage.stop();
+							
 
 							if(callback != null)
 							{
 								callback.onFinished();
 							}
-
-							return;
 						}
-					}
+					});
 				}
 			};
 			thread.start();
 
 		}
 	}
-
-	private static void setInfiniteImg(final String img, Callback callback)
-	{
-		setOneTimeImg(img, 0, callback);
-	}
-
 
 	public static void setContext(Context c)
 	{
@@ -114,101 +85,101 @@ public class ImageHelper {
 
 	public static void setGrandmaTypeHelp(Callback callback)
 	{
-		setInfiniteImg("grandma_help.gif", callback);
+		setImage("grandma_help.gif", callback);
 	}
 
 	public static void setGrandmaTypeMusic(Callback callback)
 	{
-		setInfiniteImg("grandma_music_short.gif", callback);
+		setImage("grandma_music_short.gif", callback);
 	}
 
 	public static void setGrandmaTypeHungry(Callback callback)
 	{
-		setInfiniteImg("grandma_hungry.gif", callback);
+		setImage("grandma_hungry.gif", callback);
 	}
 
 	public static void setGrandmaTypeIll(Callback callback)
 	{
-		setInfiniteImg("grandma_ill.gif", callback);
+		setImage("grandma_ill.gif", callback);
 	}
 
 	public static void setGrandmaTypeCooking(Callback callback)
 	{
-		setOneTimeImg("grandma_cook.gif", 10, callback);
+		setImage("grandma_cook.gif", callback);
 	}
 
 	public static void setGrandmaTypeWashing(final Callback callback)
 	{
-		setOneTimeImg("grandma_washing.gif",10, callback);
+		setImage("grandma_washing.gif", callback);
 	}
 
 	public static void setGrandmaTypeInCar(Callback callback)
 	{
-		setInfiniteImg("grandma_incar.gif", callback);
+		setImage("grandma_incar.gif", callback);
 	}
 
 	public static void setGrandmaTypeMedicine(Callback callback)
 	{
-		setOneTimeImg("grandma_medicine.gif", 6, callback);
+		setImage("grandma_medicine.gif", callback);
 	}
 
 	public static void setGrandmaTypeCleanDishes(Callback callback)
 	{
-		setInfiniteImg("grandma_clean_dishes.gif", callback);
+		setImage("grandma_clean_dishes.gif", callback);
 	}
 
 	public static void setGrandmaTypeShopping(Callback callback)
 	{
-		setInfiniteImg("grandma_shopping.gif", callback);
+		setImage("grandma_shopping.gif", callback);
 	}
 
 	public static void setGrandmaTypeTired(Callback callback)
 	{
-		setInfiniteImg("grandma_tired.gif", callback);
+		setImage("grandma_tired.gif", callback);
 	}
 
 	public static void setGrandmaTypeThirsty(Callback callback)
 	{
-		setInfiniteImg("grandma_thirsty.gif", callback);
+		setImage("grandma_thirsty.gif", callback);
 	}
 
 	public static void setGrandmaTypePills(Callback callback)
 	{
-		setInfiniteImg("grandma_pills.gif", callback);
+		setImage("grandma_pills.gif", callback);
 	}
 
 	public static void setGrandmaTypeSleep(Callback callback)
 	{
-		setOneTimeImg("grandma_gosleep.gif", 3, callback);
+		setImage("grandma_gosleep.gif", callback);
 	}
 
 	public static void setGrandmaTypeCleanCar(Callback callback)
 	{
-		setOneTimeImg("grandma_clean_car.gif", 5, callback);
+		setImage("grandma_clean_car.gif", callback);
 	}
 
 	public static void setGrandmaTypeWalk(Callback callback)
 	{
-		setInfiniteImg("grandma_take_walk.gif", callback);
+		setImage("grandma_take_walk.gif", callback);
 	}
 
 	public static void setGrandmaTypeEat(Callback callback)
 	{
-		setOneTimeImg("grandma_food_heavy.gif", 6, callback);
+		setImage("grandma_food_heavy.gif", callback);
 	}
 
 	public static void setGrandmaTypeDrink(Callback callback)
 	{
-		setOneTimeImg("grandma_drink.gif", 5, callback);
+		setImage("grandma_drink.gif", callback);
 	}
 
 	public static void setGrandmaTypeDoCleanDishes(Callback callback)
 	{
-		setOneTimeImg("grandma_do_clean_dishes.gif", 10, callback);
+		setImage("grandma_do_clean_dishes.gif", callback);
 	}
 
 	public static void setGrandmaTypeGoShopping(Callback callback)
 	{
-		setOneTimeImg("grandma_take_walk.gif", 10, callback);
+		setImage("grandma_take_walk.gif", callback);
 	}
 }
