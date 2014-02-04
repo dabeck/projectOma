b0c9988f351f84ba92b9ddba85c70f0382eeb561
muellerp <mw@live.de>
2014-02-04 19:46:55 +0100

    DoneReceiver vorbereitet

---
+++ src/de/unikassel/projectoma/reciever/DoneReciever.java
@@ -0,0 +1,15 @@
+package de.unikassel.projectoma.reciever;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class DoneReciever extends BroadcastReceiver {
+    
+	@Override
+	public void onReceive(Context context, Intent intent) {
+	    // TODO Auto-generated method stub
+	    
+	}
+	
+}
